4d77a5ff5af5dcefe40af785f22d8938293ef815
Soham Bal <243838957+soham-29B@users.noreply.github.com>
2026-09-25 11:51:06 +0530

    Add CARTO API key to map tiles

--- frontend/src/MapView.js
+++ frontend/src/MapView.js
@@ -74,7 +74,7 @@ function MapView({ vehicles, selectedVehicle, routePoints, focusVehicle }) {
           style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            url={`https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=${import.meta.env.VITE_CARTO_API_KEY}`}
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
         {focusVehicle && <RecenterOnVehicle vehicle={selectedVehicle} />}
